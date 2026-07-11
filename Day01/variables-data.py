50e222b7607ca16cb75e2aa8aea55f463e44f186
ARIFBILLAH1110 <sheikhabkamran@gmail.com>
2026-07-11 15:22:30 +0600

    first commit

---
+++ Day01/variables-data.py
@@ -0,0 +1,10 @@
+name = "Arif"
+age = 25
+height = 5.6
+is_student = True
+
+print(type(name))      # Output: <class 'str'>
+print(type(age))       # Output: <class 'int'>    
+print(type(height))    # Output: <class 'float'>
+print(type(is_student)) # Output: <class 'bool'>
+
